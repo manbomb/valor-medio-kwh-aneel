b22c41e45ca322700afa33c18755546050bdcbff
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-06-03 14:56:01 -0300

    test: add new test cases to pass razao_social

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,7 +5,7 @@ describe("Dias faturados", () => {
     const calculator = new ValorMedioKwhAneelCalculo();
 
     beforeEach(async () => {
-        console.time('teste');
+        console.time("teste");
     });
 
     it("deve retornar 30 dias, entre 2024-06-12 até 2024-07-12", async () => {
@@ -20,8 +20,8 @@ describe("Dias faturados", () => {
             modalidadeTarifaria: EModalidadeTarifaria.Convencional,
             subClasse: ESubClasse.Residencial,
             ICMS: 19 / 100,
-            COFINS: 4.6140 / 100,
-            PIS: 0.9980 / 100,
+            COFINS: 4.614 / 100,
+            PIS: 0.998 / 100,
         });
         // grupo tarifario B1 precisa especificar sub classe
         // grupo tarifario B3 nao precisa especificar sub classe
@@ -42,8 +42,57 @@ describe("Dias faturados", () => {
             modalidadeTarifaria: EModalidadeTarifaria.Convencional,
             subClasse: ESubClasse.Residencial,
             ICMS: 19 / 100,
-            COFINS: 4.6140 / 100,
-            PIS: 0.9980 / 100,
+            COFINS: 4.614 / 100,
+            PIS: 0.998 / 100,
+        });
+
+        console.log("resultado >", resultado);
+
+        expect(resultado.diasFaturados).toBe(30);
+        expect(resultado.bandeirasIncidentes[0]).toBeDefined();
+        expect(resultado.bandeirasIncidentes[0]!.bandeira).toBe("Verde");
+        expect(resultado.bandeirasIncidentes[1]).toBeDefined();
+        expect(resultado.bandeirasIncidentes[1]!.bandeira).toBe("Verde");
+    });
+
+    it("deve retornar 30 dias, entre 2024-06-12 até 2024-07-12, com razao social", async () => {
+        const inicio = new Date("2024-06-12");
+        const fim = new Date("2024-07-12");
+
+        const resultado = await calculator.calc({
+            dataInicio: inicio,
+            dataFim: fim,
+            cnpjDistribuidora: "04368898000106",
+            sigAgente: "Neoenergia PE",
+            subGrupoTarifario: ESubGrupoTarifario.B1,
+            modalidadeTarifaria: EModalidadeTarifaria.Convencional,
+            subClasse: ESubClasse.Residencial,
+            ICMS: 19 / 100,
+            COFINS: 4.614 / 100,
+            PIS: 0.998 / 100,
+        });
+        // grupo tarifario B1 precisa especificar sub classe
+        // grupo tarifario B3 nao precisa especificar sub classe
+        console.log("resultado >", resultado);
+
+        expect(resultado.diasFaturados).toBe(30);
+    });
+
+    it("deve retornar bandeira verde para o periodo, com razao social", async () => {
+        const inicio = new Date("2025-03-08");
+        const fim = new Date("2025-04-07");
+
+        const resultado = await calculator.calc({
+            dataInicio: inicio,
+            dataFim: fim,
+            cnpjDistribuidora: "04368898000106",
+            sigAgente: "Neoenergia PE",
+            subGrupoTarifario: ESubGrupoTarifario.B1,
+            modalidadeTarifaria: EModalidadeTarifaria.Convencional,
+            subClasse: ESubClasse.Residencial,
+            ICMS: 19 / 100,
+            COFINS: 4.614 / 100,
+            PIS: 0.998 / 100,
         });
 
         console.log("resultado >", resultado);
@@ -56,6 +105,6 @@ describe("Dias faturados", () => {
     });
 
     afterEach(() => {
-        console.timeEnd('teste');
+        console.timeEnd("teste");
     });
 });
